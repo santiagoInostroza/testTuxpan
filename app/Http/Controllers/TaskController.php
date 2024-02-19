b71eee78c64808adbd4dc581abff1a570f044887
Santiago Inostroza <santiagoinostroza2@gmail.com>
2024-02-19 00:08:28 -0300

    Se agregan resource y form-request a TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,33 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use App\Http\Resources\TaskResource;
+use App\Http\Requests\TaskCreateRequest;
+use App\Http\Requests\TaskUpdateRequest;
 
 class TaskController extends Controller
 {
     // crear para api
     // index(): Muestra una lista de tareas.
     public function index(){
-        $tasks = Task::all();
-        return response()->json($tasks, 200);
+        $tasks = Task::paginate(10);
+        return TaskResource::collection($tasks);
+       // return response()->json($tasks, 200);
     }
     // store(Request $request): Crea una nueva tarea.
-    public function store(Request $request){
-        $task = Task::create($request->all());
-        return response()->json($task, 201);
+    public function store(TaskCreateRequest $request){
+
+        $data = $request->validated();
+        $task = Task::create($data);
+        return new TaskResource($task);
     }
     
     // show(Task $task): Muestra una tarea específica.
     public function show(Task $task){
-        return response()->json($task, 200);
+        return new TaskResource($task);
+       // return response()->json($task, 200);
     }
     // update(Request $request, Task $task): Actualiza una tarea.
-    public function update(Request $request, Task $task){
-        $task->update($request->all());
-        return response()->json($task, 200);
+    public function update(TaskUpdateRequest $request, Task $task){
+        $data = $request->validated();
+        $task->update($data);
+        return response()->json(['message' => 'Task updated'], 200);
+
+        //$task->update($request->all());
+        //return response()->json($task, 200);
     }
     // destroy(Task $task): Elimina una tarea.
     public function destroy(Task $task){
         $task->delete();
-        return response()->json(null, 204);
+        return response()->json(['message' => 'Task deleted'], 200);
     }
 }
